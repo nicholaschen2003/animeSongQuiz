05fde119bbc826fc9b2d3e4111e3d69dfbec5b0c
Nicholas Chen <nicholas.chen.21@bishops.com>
2020-05-08 09:39:41 -0700

    added views and templates to get num players and info

--- songQuiz/urls.py
+++ songQuiz/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'songQuiz'
 urlpatterns = [
-    path('', views.startPage, name='startPage'),
+    path('', views.getNumUsers, name='getNumUsers'),
+    path('getPlayerData/', views.getPlayerData, name='getPlayerData'),
+    path('createPlayers/', views.createPlayers, name='createPlayers'),
 ]
--- songQuiz/views.py
+++ songQuiz/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-def startPage(request):
+def getNumUsers(request):
+
+    return render(request, 'songQuiz/getNumUsers.html')
+
+def getPlayerData(request):
+
+    context = {
+        'numPlayers': request.POST['numUsers']
+    }
+
+    return render(request, 'songQuiz/getPlayerData.html', context)
+
+def createPlayers(request):
+
+    for i in range(len(request.POST)-1):
+        print(request.POST[str(i+1)])
 
     return HttpResponse("placeholder.")
