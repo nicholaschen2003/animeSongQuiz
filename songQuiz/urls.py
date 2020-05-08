44852437bf8a3b9e41d85cbd17335c2d74474dc1
Nicholas Chen <nicholas.chen.21@bishops.com>
2020-05-08 10:07:09 -0700

    added static to url config

--- songQuiz/urls.py
+++ songQuiz/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path('', views.getNumUsers, name='getNumUsers'),
     path('getPlayerData/', views.getPlayerData, name='getPlayerData'),
     path('createPlayers/', views.createPlayers, name='createPlayers'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
