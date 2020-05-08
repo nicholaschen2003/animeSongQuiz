facd8c3946cfc01134b72344a727bf2d6752fafa
Nicholas Chen <nicholas.chen.21@bishops.com>
2020-05-07 23:15:08 -0700

    added basic models and temporary view

--- songQuiz/models.py
+++ songQuiz/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Song(models.Model):
+    name = models.CharField(max_length=200, default=None)
+    file_path = models.CharField(max_length=200, default=None)
+    difficulty = models.CharField(max_length=200, default=None)
+    points = models.IntegerFiels(default=0)
+    times_played = models.IntegerField(default=0)
+    times_correct = models.IntederField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length=200, default=None)
+    points = models.IntegerField(default=0)
+    songs_played = models.CharField(default=None) #also holds data on times played and times correct for each song played
+
+    def __str__(self):
+        return self.name
